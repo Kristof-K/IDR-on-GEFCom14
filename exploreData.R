79f13edbc3b07466ee3248687c0e4e6cbe61cdb9
Kristof-K <kristofkraus@gmail.com>
2021-08-14 18:50:08 +0200

    Added results plot for slides for load and price.

--- exploreData.R
+++ exploreData.R
@@ -212,6 +212,15 @@ examineLoad <- function() {
     facet_wrap(~Temperature) +
     geom_point()
   }
+  data <- loadLoad(15)
+  data$Zone1$Train %>% mutate(Hour = hour(TIMESTAMP), Year = year(TIMESTAMP),
+                              Month = month(TIMESTAMP)) %>%
+    group_by(Hour, Year, Month) %>% summarise(averageTmp = mean(w10),
+                                              .groups="drop") %>%
+    ggplot(aes(x=Month, y=averageTmp, color=as.factor(Year))) +
+    facet_wrap(~Hour) +
+    geom_line() +
+    ggtitle("Average monthly temperature of w10 for every specific hour")
 }
 
 examinePrice <- function() {
@@ -260,14 +269,16 @@ plotsForSlides <- function() {
                           0.014288, 0.014294, 0.0149964, 0.0154773, 0.01549,
                           0.015977, 0.0165608, 0.0166733, 0.01755, 0.0178692,
                           0.0184789, 0.0208583, 0.02128, 0.025524, 0.0317609,
-                          0.0375917, 0.014839771, 0.013732111),
-                  group=c(rep("par", 20), "ben", rep("idr", 2)),
+                          0.0375917, 0.01995319, 0.014839771, 0.013732111),
+                  group=c(rep("par", 20), "ben", rep("idr", 3)),
                   name=c(paste0(1:20, c("st", "nd", "rd", rep("th", 17))),
-                         "Benchmark", "IDR hours 1", "IDR hours 5"),
-                  label=c(rep("", 20), "",
-                          "1 variable", "5 variables\n+ subagging")) %>%
+                         "Benchmark", "IDR", "IDR hours 1", "IDR hours 5"),
+                  label=c(rep("", 20), "", "IDR on\nradiation",
+                          "IDR on\nradiation\ngrouped\nby hours",
+                          "5 variables\n+ hour groups\n+ subagging")) %>%
     arrange(desc(score)) %>%
-    mutate(name = factor(name, ordered=TRUE, levels=name))
+    mutate(name = factor(name, ordered=TRUE, levels=name),
+           index=1:length(score))
 
   W <- data.frame(score=c(0.03711091, 0.03827364, 0.03831, 0.03834417,
                           0.0389725, 0.03946167, 0.04149818, 0.04473917,
@@ -276,28 +287,63 @@ plotsForSlides <- function() {
                           0.04307092),
                   group=c(rep("par", 12), "ben", rep("idr", 4)),
                   name=c(paste0(1:12, c("st", "nd", "rd", rep("th", 9))),
-                         "Benchmark", "IDR all", "IDR hours", "IDR seasons",
-                         "IDR wind angle"),
-                  label=c(rep("", 12), "", "1 variable\n+ different grouping",
+                         "Benchmark", "IDR", "IDR by hours", "IDR by seasons",
+                         "IDR by wind angle"),
+                  label=c(rep("", 12), "",
+                          "IDR on\nwind speed at 100m\n+ different grouping",
                           rep("", 3))) %>%
     arrange(desc(score)) %>%
-    mutate(name = factor(name, ordered=TRUE, levels=name))
+    mutate(name = factor(name, ordered=TRUE, levels=name),
+           index=1:length(score))
+  
+  P <- data.frame(score=c(2.72260416, 2.73264, 2.82480333, 3.3393825,
+                          3.38856916, 3.67968, 4.15617545, 4.46310583,
+                          4.48946636, 5.08586916, 5.20363454, 5.2952075,
+                          7.7270825, 8.86687636, 10.58733333, 19.46707916,
+                          5.219633, 4.051974, 4.027405),
+                  group=c(rep("par", 15), "ben", rep("idr", 3)),
+                  name=c(paste0(1:15, c("st", "nd", "rd", rep("th", 12))),
+                         "Benchmark", "IDR", "seasons", "seasons+\nhours*"),
+                  label=c(rep("", 17),
+                          "IDR on\n1 variable\n+ different grouping",
+                          rep("", 1))) %>%
+    arrange(desc(score)) %>%
+    mutate(name = factor(name, ordered=TRUE, levels=name),
+           index=1:length(score))
+  
+  L <- data.frame(score=c(7.197126, 7.23131727, 7.45392166, 7.50520916,
+                          7.535513, 7.95646833, 8.466394, 8.58871,
+                          8.62505818, 8.93657583, 9.26274090, 9.407766,
+                          9.48056416, 9.5641, 10.00587875, 10.25994833,
+                          10.62373454, 11.50976555, 11.76980833, 12.17955666,
+                          13.32187909, 15.5810025, 9.584264),
+                  group=c(rep("par", 21), "ben", rep("idr", 1)),
+                  name=c(paste0(1:21, c("st", "nd", "rd", rep("th", 18))),
+                         "Benchmark", "IDR by months"),
+                  label=c(rep("", 22),
+                          "IDR on\n1 variable\n+ different grouping")) %>%
+    arrange(desc(score)) %>%
+    mutate(name = factor(name, ordered=TRUE, levels=name),
+           index=1:length(score))
 
-  plotResults <- function(data, track) {
+  plotResults <- function(data, track, f=0.5) {
     ggplot(data) +
-      geom_col(aes(x=name, y=score, fill=group), show.legend=FALSE,
+      geom_col(aes(x=factor(index), y=score, fill=group), show.legend=FALSE,
               position="dodge2") +
       xlab("") +
       ylab("") +
       ggtitle(paste("Mean pinball scores", track, "track")) +
       theme_bw() +
       theme(text = element_text(size = 15),
-            axis.text.x = element_text(angle = 90)) +
-      geom_text(aes(x=name, y=score + 0.5 * score * (score <= max(score)*2/3),
-                    label=label, color=group), , size=5, show.legend=FALSE) +
-      ggsave(paste0("Scores_", track, ".pdf"), path="../WorkshopIAI/",
-             width=10, height=6)
+            axis.text.x = element_blank()) +
+      geom_text(aes(x=index, y=score + f * score * (score <= max(score)*1/(f*3)),
+                    label=label, color=group), size=5, show.legend=FALSE) +
+      geom_text(aes(x=index, y=score/2, label=name), angle=90) +
+      ggsave(paste0("Scores_", track, ".pdf"), path="../Overview",
+             width=10, height=5)
   }
   plotResults(S, "solar")
   plotResults(W, "wind")
+  plotResults(P, "price", f=1.5)
+  plotResults(L, "load")
 }
